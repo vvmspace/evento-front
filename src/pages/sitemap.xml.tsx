48ff98818088e79904952208a018c16f841009c6
Vladimir V.M <vladimir.myagdeev@noveogroup.com>
2023-09-11 04:51:08 +0400

    feat<seo>: sitemap + robots.txt

---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,57 @@
+// sitemap.xml.tsx
+
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import { fetch } from "next/dist/compiled/@edge-runtime/primitives";
+import { Event } from "../models/event.model";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
+import { performGroupAliasFromEvent } from "@/components/EventCard/EventCard";
+import group from "@/pages/[group]";
+
+type SitemapProps = {
+    events: Event[];
+}
+
+const Sitemap = ({ events }: SitemapProps) => {
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ locale, res }) => {
+    const renderEventUrl = (event: Event) => {
+        const group = performGroupAliasFromEvent(event);
+        const alias = event.alias;
+        return `<url>
+                <loc>${process.env.NEXT_PUBLIC_BASE_URL}/${group}/${alias}</loc>
+                <lastmod>${event.updatedAt}</lastmod>
+            </url>`;
+    }
+
+    const renderGroupUrl = (group: string) => {
+        return `<url>
+                <loc>${process.env.NEXT_PUBLIC_BASE_URL}/${group}</loc>
+            </url>`;
+    }
+
+    const renderSitemap = (events: Event[]) => {
+        return `<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+                ${[...(new Set(events.map(event => performGroupAliasFromEvent(event) as string)))].map((group => renderGroupUrl(group)))}
+                ${events.map(event => renderEventUrl(event))}
+            </urlset>`;
+    }
+
+    const everywhere_url = `${process.env.API_PREFIX}/events?active=true&ssr=true&select=country,genre,updatedAt,image,name,alias,start,price_min,price_max,title,call_for_action,venue,provider_id,provider_internal_venue_address,price_currency&ssr=true&size=10000&sort=start_asc`;
+    const response = await fetch(everywhere_url);
+    const events: Event[] = await response.json();
+
+    const sitemap = renderSitemap(events);
+
+    res.setHeader('Content-Type', 'text/xml');
+    res.write(sitemap);
+    res.end();
+    return {
+        props: {}
+    }
+}
+
+export default Sitemap;
