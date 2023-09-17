21fee96277cf6c87af8bd0a031f227193473e752
Vladimir V.M <vladimir.myagdeev@noveogroup.com>
2023-09-17 17:50:42 +0400

    feat<seo>:blur

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -55,6 +55,7 @@ const EventCard: React.FC<EventCardProps> = ({ event }) => {
             height={225}
             layout="responsive"
             loading={"lazy"}
+            placeholder={"blur"}
           />
           <h2 className={styles.cardTitle}>
             {event.name[currentLanguage] ?? event.name["en"]}
