1f38758ccb7405792ad61ba3a675f107c8922321
Vladimir V.M <vladimir.myagdeev@noveogroup.com>
2023-09-18 14:46:59 +0400

    feat<seo>: Sitemap: ${URL_PREFIX}sitemap.xml

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,4 @@
 rm ./public/robots.txt
 cp ./public/robots.main.txt ./public/robots.txt
+echo "Sitemap: ${URL_PREFIX}sitemap.xml" >>./public/robots.txt
 echo "Host: $URL_PREFIX" >>./public/robots.txt
