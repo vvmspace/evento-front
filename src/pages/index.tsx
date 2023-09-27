a1fd296a76c883e8e86f69d5dbdc9c2bffdd93b5
Vladimir V.M <vladimir.myagdeev@noveogroup.com>
2023-09-28 03:56:54 +0400

    feat<front>: start_from

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,7 +38,9 @@ const HomePage: FC<Props> = ({ latest, top, title }) => {
 
 export async function getStaticProps() {
   const latest_response = await fetch(
-    `${process.env.API_PREFIX}/events?active=true&select=group_alias,city_name,country,provider_city_name,genre,updatedAt,image,name,alias,start,price_min,price_max,title,call_for_action,venue,provider_id,provider_internal_venue_address,price_currency&distinct=provider_city_name&ssr=true&size=8&use_cache=true&sort=updatedAt_desc&locale=${DEFAULT_LANGUAGE}`,
+    `${process.env.API_PREFIX}/events?active=true&select=group_alias,city_name,country,provider_city_name,genre,updatedAt,image,name,alias,start,price_min,price_max,title,call_for_action,venue,provider_id,provider_internal_venue_address,price_currency&distinct=provider_city_name&ssr=true&size=8&use_cache=true&start_from=${
+        new Date().toISOString().split("T")[0]
+    }&sort=start_asc&locale=${DEFAULT_LANGUAGE}`,
     {
       next: {
         revalidate: 24 * 60 * 60,
