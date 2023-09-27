7f2417d8c926cfeb142e12eb3019e12e3e31e7b9
Vladimir V.M <vladimir.myagdeev@noveogroup.com>
2023-09-28 03:02:03 +0400

    feat<related>: related from current date

--- src/pages/[group]/[alias].tsx
+++ src/pages/[group]/[alias].tsx
@@ -28,7 +28,9 @@ const getRelated = async (group: string) => {
   if (cachedRelated[group]) {
     return cachedRelated[group];
   }
-  const everywhere_url = `${process.env.API_PREFIX}/events?use_cache=true&active=true&ssr=true&select=group_alias,provider_city_name,country,genre,updatedAt,image,name,alias,start,price_min,price_max,title,call_for_action,venue,provider_id,provider_internal_venue_address,price_currency&ssr=true&size=4&everywhere=${group}&sort=start_asc&locale=${process.env.NEXT_PUBLIC_DOMAIN_LANGUAGE}`;
+  const everywhere_url = `${process.env.API_PREFIX}/events?use_cache=true&active=true&ssr=true&select=group_alias,provider_city_name,country,genre,updatedAt,image,name,alias,start,price_min,price_max,title,call_for_action,venue,provider_id,provider_internal_venue_address,price_currency&ssr=true&size=4&everywhere=${group}&sort=start_asc&start_from=${
+    new Date().toISOString().split("T")[0]
+  }&locale=${process.env.NEXT_PUBLIC_DOMAIN_LANGUAGE}`;
   const group_response = await fetch(everywhere_url);
   console.log("everywhere_url", everywhere_url);
   const related: Event[] = await group_response.json();
