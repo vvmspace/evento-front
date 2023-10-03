791920bb45aa99d01a670689df037288976e0d61
Vladimir V.M <vladimir.myagdeev@noveogroup.com>
2023-10-03 22:47:11 +0400

    fix<ssg>: fallback: 'blocking'

--- src/pages/[group]/index.tsx
+++ src/pages/[group]/index.tsx
@@ -144,7 +144,7 @@ export async function getStaticPaths() {
       params: { group },
     }));
 
-  return { paths, fallback: true };
+  return { paths, fallback: 'blocking' };
 }
 
 export async function getStaticProps(context: { params: { group: string } }) {
